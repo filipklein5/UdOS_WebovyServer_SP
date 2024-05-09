a084acd05a0b04872d809a07442fc9f00521eb70
Filip Klein <klein5@stud.uniza.sk>
2024-05-09 09:53:21 +0200

    zmeny...

--- firewall.sh
+++ firewall.sh
@@ -2,6 +2,7 @@
 
 # Allow incoming traffic on ports 443 and 2222
 sudo iptables -A INPUT -p tcp --dport 22 -j ACCEPT
+sudo iptables -A INPUT -p tcp --dport 80 -j ACCEPT
 sudo iptables -A INPUT -p tcp --dport 443 -j ACCEPT
 sudo iptables -A INPUT -p tcp --dport 2222 -j ACCEPT
 
