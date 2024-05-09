b4a1c2ce7d9a31c249d29cf94a3fdfca7c9b6bb5
Filip Klein <klein5@stud.uniza.sk>
2024-05-09 10:12:14 +0200

    zmenene 80 na 443

--- konfig_web_servera.sh
+++ konfig_web_servera.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 
-# nastavenie virtualneho hosta pre Apache
+# nastavenie virtualneho hosta pre Apache, ktory pocuva na porte 443
 konfiguraciaVirtualnehoHosta() {
     local domain_name=$1
     local document_root=$2
     local config_file="/etc/apache2/sites-available/$domain_name.conf"
 
     cat << EOF | sudo tee "$config_file" >/dev/null
-<VirtualHost *:80>
+<VirtualHost *:443>
     ServerAdmin webmaster@$domain_name
     ServerName $domain_name
     DocumentRoot $document_root
     ErrorLog ${APACHE_LOG_DIR}/error.log
     CustomLog ${APACHE_LOG_DIR}/access.log combined
+    SSLEngine on
+    SSLCertificateFile /etc/ssl/certs/ssl-cert-snakeoil.pem
+    SSLCertificateKeyFile /etc/ssl/private/ssl-cert-snakeoil.key
 </VirtualHost>
 EOF
 
@@ -43,6 +46,9 @@ konfiguraciaVirtualnehoHosta "localhost" "/var/www/html/udos_webserver/"
 
 # nastavenie pristupovych prav pre adresare
 nastavenieAdresarovychPrav "/var/www/html/udos_webserver/"
+
+#nakopirovanie defaultnej HTML stranky do vytvoreneho priecinka
+sudo cp /UdOS_WebovyServer_SP/index.html /var/www/html/udos_webserver/
 
 echo "Nastavenie webového servera bolo dokončené."
 
